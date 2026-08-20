251fd4478fdd43b8d6515de5dba6a8a3b9b43d8d
Operative88 <wojciechkocemba21@gmail.com>
2026-08-20 15:04:28 +0200

    repair syntax at line 30

--- log-archive.sh
+++ log-archive.sh
@@ -1,35 +1,39 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# log-archive - narzędzie CLI do archiwizacji logów
+
 
 set -euo pipefail
 
-if [ "$#" -ne 1 ] then;
-echo "Użycie: "log-archive <katalog-z-logami>"
-exit 1
+
+if [ "$#" -ne 1 ]; then
+    echo "Użycie: log-archive <katalog-z-logami>"
+    exit 1
 fi
 
 LOG_DIR="$1"
 
-if [ ! -d "$LOG_DIR" ] then;
-echo "katalog '$LOG_DIR' jest nieprawidłowy lub nie istnieje"
-exit 1
+
+if [ ! -d "$LOG_DIR" ]; then
+    echo "Błąd: '$LOG_DIR' nie jest katalogiem lub nie istnieje."
+    exit 1
 fi
 
-ARCHIVE_DIR="${LOG_DIR%/}/archives}"
+
+ARCHIVE_DIR="${LOG_DIR%/}/archives"      # podkatalog na archiwa
 mkdir -p "$ARCHIVE_DIR"
 
-TIMESTAMP=$(date +"%y%m%d_%H%M%S")
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")        # np. 20240816_100648
 ARCHIVE_NAME="logs_archive_${TIMESTAMP}.tar.gz"
 ARCHIVE_PATH="${ARCHIVE_DIR}/${ARCHIVE_NAME}"
 
+
+# -c: utwórz, -z: gzip, -f: plik wyjściowy, -C: wejdź do katalogu przed pakowaniem
+# --exclude="archives": nie pakuj katalogu z archiwami
 tar -czf "$ARCHIVE_PATH" -C "$LOG_DIR" --exclude="archives" .
-ARCHIVE_PATH="${ARCHIVE_DIR}/${ARCHIVE_NAME}"
+
 
 LOG_FILE="${ARCHIVE_DIR}/archive.log"
 echo "$(date +"%Y-%m-%d %H:%M:%S") - Utworzono archiwum: $ARCHIVE_PATH" >> "$LOG_FILE"
 
 echo "Archiwum utworzone: $ARCHIVE_PATH"
-
-
-
-
-
